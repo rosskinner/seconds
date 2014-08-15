9df2aebf4c80a2185d3a27f38f67c5aa2566f64e
Rosalind Skinner <ros.skinner@gmail.com>
2014-08-15 14:53:51 +1000

    config environments production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -31,6 +31,7 @@ Rails.application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+
 
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
