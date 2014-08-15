8fa1d26fc5a624ed330261f01092e151e2b142d4
Rosalind Skinner <ros.skinner@gmail.com>
2014-08-15 12:36:47 +1000

    change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # get 'pages#index'
-  devise_scope :user do
-    root :to => "devise/sessions#new"
-  end
+  root :to => "pages#index"
+  # devise_scope :user do
+  #   root :to => "devise/sessions#new"
+  # end
   devise_for :users
   # root :to => 'devise/sessions#new'
   resources :videos
