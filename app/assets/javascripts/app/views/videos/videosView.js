9076dd57fd950e60d428ead63e6068dc3a701acb
Rosalind Skinner <ros.skinner@gmail.com>
2014-08-20 10:08:26 +1000

    videoesView backbone clear and fetch

--- app/assets/javascripts/app/views/videos/videosView.js
+++ app/assets/javascripts/app/views/videos/videosView.js
@@ -3,16 +3,23 @@ var app = app || {};
 app.VideosView = Backbone.View.extend({
   el: '.grid-video',
   initialize: function() {
-    this.render();
+    // this.render();
+    this.fetchVideo();
     console.log("rendering to page");
   },
   render: function () {
+    this.$el.html('');
     var video_list = Handlebars.compile(app.templates.videosTemplate);
     var view = this;
     this.collection.each(function(videos) {
       var copy = video_list(videos.toJSON() )
-      view.$el.append( copy );
+      view.$el.prepend( copy );
     })
+  },
+  fetchVideo: function() {
+    this.fetch().done(function (){
+      this.render();
+    });
   }
   // view: function(e) {
   //   var inside = (e.target.id);
